2d9f542a3d438ea167a3f1f5d3070ef4d903b611
TerrafirmaCharlie <71718048+TerrafirmaCharlie@users.noreply.github.com>
2020-11-20 12:02:31 +0000

    Remade github listener
    #r Had to remake it because changing the file names messed up the other one

---
+++ commits_reader.py
@@ -0,0 +1,52 @@
+from github import Github
+import markdown2
+
+access_token = "8b7d51a8152ae5a842acfc5b747c15f0eb156a07"
+g = Github(access_token)
+
+
+def font_colour_picker(colour_font_desc):
+    map_dict = {"#r": "color:red;", "#g": "color:#00cc00;", "#b": "color:blue;", "#y": "color:black",
+                "# ": "color:black"}
+    description_stripped = colour_font_desc.strip(colour_font_desc[:2])
+    font_colour = map_dict[colour_font_desc[:2]]
+    new_description = f"<span style=\"{font_colour}\"> {description_stripped}</span>"
+    return new_description
+
+
+repo_find = g.get_repo("TerrafirmaCharlie/github_listener")
+repo_desc = repo_find.description
+commits = repo_find.get_commits()
+
+with open("C:\\Users\\My Desktop\\Documents\\GitHub\\github_listener\\commits.md", 'a') as f:
+    f.truncate(0)
+
+    for commit in commits:
+        author = commit.commit.committer.name
+        date = commit.stats.last_modified
+        summary_description = commit.commit.message
+        splitting = summary_description.splitlines()
+        summary = splitting[0]
+        description = "".join(splitting[1:])
+        if "#" in description[:2]:
+            description = font_colour_picker(description)
+
+        f.write("\n")
+        f.write(f"####Author:\n {author}\n")
+        f.write(f"####Date Modified:\n {date}\n")
+        f.write(f"####Summary:\n {summary}\n")
+        f.write(f"####Description:\n {description}\n")
+        f.write("<hr>\n")
+
+md_to_html = markdown2.markdown_path("C:\\Users\\My Desktop\\Documents\\GitHub\\github_listener\\commits.md")
+
+Html_file = open("commits.html", "w")
+Html_file.write(md_to_html)
+
+# DESCRIPTION SORTING
+# #r_s = red and small   #g_s = green and small  #b_s = blue and small
+# #r_m = red and medium  #g_m = green and medium  #b_m = blue and medium
+# #r_b = red and big     #g_b = green and big  #b_b = blue and big
+
+
+
